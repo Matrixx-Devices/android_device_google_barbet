3cfa99cba058ffb7c92e91cd3e2a853b26fbec0f
Bobby306 <143777624+Bobby306@users.noreply.github.com>
2023-12-20 17:16:03 -0500

    Disable crDroid artifact/partition GMS
    Adds env variables to disable artifact path requirements (to build crdroid) and increase system/product partitions for gapps support

--- lineage_barbet.mk
+++ lineage_barbet.mk
@@ -6,6 +6,8 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+WITH_GMS := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -28,5 +30,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="barbet-user 13 TQ3A.230901.001 10750268 release-keys"
 
 BUILD_FINGERPRINT := google/barbet/barbet:13/TQ3A.230901.001/10750268:user/release-keys
+
 
 $(call inherit-product, vendor/google/barbet/barbet-vendor.mk)
