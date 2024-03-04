eb36832b7c5b65fe8fe0fb0d570e7390055aa80a
Chirayu Desai <chirayudesai1@gmail.com>
2024-03-05 01:35:05 +0530

    proprietary-files: AP1A.240305.019.A1
    Change-Id: Ibe07ee6d4f71f6c681993a323841ebca9d236d97

--- device-lineage.mk
+++ device-lineage.mk
@@ -6,3 +6,20 @@
 
 $(call inherit-product, device/google/redbull/device-lineage.mk)
 
+# Nos
+PRODUCT_PACKAGES += \
+    libnos:64 \
+    libnosprotos:64 \
+    libnos_client_citadel:64 \
+    libnos_datagram:64 \
+    libnos_datagram_citadel:64 \
+    libnos_feature:64 \
+    libnos_transport:64 \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_keymaster_ctdl:64 \
+    nos_app_weaver:64
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi-V2-ndk.vendor:64
